ab0aa0d27f145dd70002c895a780cd9370f76ccd
Asfak04 <asfakshaikh287@gmail.com>
2025-11-05 19:00:01 +0530

    commit

---
+++ Projects/Auto_Portal/admin/sidemenu.php
@@ -0,0 +1,28 @@
+<img src="./images/dashboard.png" alt="" class="rounded-5 img-fluid w-50">
+
+<ul class="sidebar-links">
+        <li><a href="admin_dashboard.php" class="text-dark"><span class="class bi bi-speedometer text-dark"></span> Dashboard <span class="bi bi-chevron-left float-end"></span>
+                </a> 
+        </li>
+        <li><a href="Vehicles.php" class="text-dark"><span class="class bi bi-car-front text-dark"></span> Vehicles <span class="bi bi-chevron-left float-end"></span>
+                </a>
+        </li>
+        <li><a href="buyers.php" class="text-dark"><span class="class bi bi-person  text-dark"></span> Buyers <span class="bi bi-chevron-left float-end"></span>
+                </a> 
+        </li>        
+        <li><a href="sellers.php" class="text-dark"><span class="class bi bi-person-badge text-dark"></span> Sellers <span class="bi bi-chevron-left float-end"></span>
+                </a></li>
+        <li><a href="users.php" class="text-dark"><span class="class bi bi-person-circle text-dark"></span> Users <span class="bi bi-chevron-left float-end"></span>
+                </a>
+        </li>
+        <li><a href="reports.php" class="text-dark"><span class="class bi bi-file-earmark-bar-graph text-dark"></span> Reports <span class="bi bi-chevron-left float-end"></span>
+                </a>
+        </li>
+        <li><a href="Listings.php" class="text-dark"><span class="class bi bi-card-list text-dark"></span> Listings <span class="bi bi-chevron-left float-end"></span>
+                </a>
+        </li>
+        <li><a href="settings.php" class="text-dark"><span class="class bi-gear text-dark"></span> Settings <span class="bi bi-chevron-left float-end"></span>
+                </a> 
+        </li>
+
+</ul>
