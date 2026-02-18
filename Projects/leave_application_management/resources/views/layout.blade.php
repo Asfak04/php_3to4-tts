06f724e4b372ffda6b8284ac313378ced9731b8c
Asfak04 <asfakshaikh287@gmail.com>
2026-02-18 15:48:16 +0530

    commit

--- Projects/leave_application_management/resources/views/layout.blade.php
+++ Projects/leave_application_management/resources/views/layout.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Library Management System</title>
+    <title>Leave Management</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Bootstrap 5 -->
